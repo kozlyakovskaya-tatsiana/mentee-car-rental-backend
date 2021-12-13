b851c9c4439fd1cc0c7e42e130338c76c6975a2e
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2021-12-13 17:07:32 +0300

    Added brand entity

---
+++ carRental/carRental.DAL/Entities/Brand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace carRental.DAL.Entities
+{
+    class Brand
+    {
+        long id { get; set; }
+        string breandName { get; set; }
+    }
+}
