18a4254490b13bf7931efdd476f91694f91ae745
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2021-12-13 17:08:20 +0300

    Added content entity

---
+++ carRental/carRental.DAL/Entities/Content.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace carRental.DAL.Entities
+{
+    class Content
+    {
+        string contentType { get; set; }
+        byte[] content { get; set; }
+    }
+}
