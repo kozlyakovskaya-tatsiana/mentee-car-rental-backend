6cbab6a9c3b35def87c55ffe400068145620ab8b
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2021-12-13 17:15:18 +0300

    Added car entity

---
+++ carRental/carRental.DAL/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace carRental.DAL.Entities
+{
+    class Car
+    {
+        long id { get; set; }
+        long carTypeId { get; set; }
+        long userId { get; set; }
+        long placeId { get; set; }
+        float pricePerHour { get; set; }
+        Content photo { get; set; }
+        string coordinates { get; set; }
+        DateTime busyTime { get; set; }
+        bool isBlocked { get; set; }
+    }
+}
