0fe039843daa910677c92ee999d54b5756cc7d31
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2021-12-13 17:08:53 +0300

    Added car type entity

---
+++ carRental/carRental.DAL/Entities/carType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace carRental.DAL.Entities
+{
+    // TODO move ENUM to different file
+    enum TransmisionType
+    {
+        auto,
+        mechanic
+    }
+    class carType
+    {
+        Brand brand { get; set; }
+        string model { get; set; }
+        float fuelConsumption { get; set; }
+        TransmisionType transmision { get; set; }
+        int quatityOfSeats { get; set; }
+    }
+}
