fd173b6940f0f9cf4fa074853b53b87b9fe21109
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2021-12-13 17:08:06 +0300

    Added country entity

---
+++ carRental/carRental.DAL/Entities/Country.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace carRental.DAL.Entities
+{
+    class Country
+    {
+        long id { get; set; }
+        string coutryName { get; set; }
+    }
+}
