06e03c412de98d265af65f516c560b5764595f3c
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2021-12-13 17:07:52 +0300

    Added place entity

---
+++ carRental/carRental.DAL/Entities/Place.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace carRental.DAL.Entities
+{
+    class Place
+    {
+        Country country { get; set; }
+        string city { get; set; }
+    }
+}
