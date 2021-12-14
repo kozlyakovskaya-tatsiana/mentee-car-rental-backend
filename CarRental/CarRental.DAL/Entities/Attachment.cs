010750e9dbc7e10c124504566565b77aa226b050
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2021-12-14 16:52:25 +0300

    Reformat files and folders

---
+++ CarRental/CarRental.DAL/Entities/Attachment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace carRental.DAL.Entities
+{
+    public class Attachment
+    {
+        public Guid Id { get; set; }
+        public Guid CarId { get; set; }
+        public Car Car { get; set; }
+        public string FileFormat { get; set; }
+        public byte[] Content { get; set; }
+    }
+}
