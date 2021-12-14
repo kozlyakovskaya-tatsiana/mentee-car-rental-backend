82f4074c794b85f255260dfb1eef5a5c83090415
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2021-12-14 16:20:26 +0300

    Rename files and folders

---
+++ CarRental/CarRental.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace carRental.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ CarRental/CarRental.DAL/Entities/FirstEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace carRental.DAL.Entities
+{
+    class FirstEntity
+    {
+    }
+}
