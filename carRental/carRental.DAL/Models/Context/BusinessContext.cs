a271aa782d9f714a706d0e69a62e8dbfce6676ae
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2021-12-14 09:36:56 +0300

    Added Business context

---
+++ carRental/carRental.DAL/Models/Context/BusinessContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace carRental.DAL.Models.Context
+{
+    class CarContext
+    {
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<CarType> CarTypes { get; set; }
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Place> Places { get; set; }
+        public DbSet<Report> Reports { get; set; }
+
+    }
+}
