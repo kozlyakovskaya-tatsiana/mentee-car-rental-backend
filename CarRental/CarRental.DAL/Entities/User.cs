cdbf7ea7b87c3284cc40e84de6164b46f45fae3c
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2021-12-15 12:39:39 +0300

    Added User Entity linked with Booking Report

---
+++ CarRental/CarRental.DAL/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace CarRental.DAL.Entities
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+
+        public List<BookingReport> Reports { get; set; }
+    }
+}
