1096212c238cf432429d9c8928b739ad238a8135
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2021-12-15 16:39:28 +0300

    Added Time properties for Rental Points

--- CarRental/CarRental.DAL/Entities/BookingReport.cs
+++ CarRental/CarRental.DAL/Entities/BookingReport.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using CarRental.Common.Enums;
 
 namespace CarRental.DAL.Entities
 {
     public class BookingReport : BaseEntity
     {
         public double Mark { get; set; }
+        public DateTime StartOfferTime { get; set; }
+        public DateTime EndOfferTime { get; set; }
+        public double Price { get; set; }
 
         public Car Car { get; set; }
         public Guid CarId { get; set; }
