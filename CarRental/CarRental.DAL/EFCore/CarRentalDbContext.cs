44c3019598f6945128050f47ea99c6abc39126c0
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2021-12-16 15:18:09 +0300

    Configured most important required properties

--- CarRental/CarRental.DAL/EFCore/CarRentalDbContext.cs
+++ CarRental/CarRental.DAL/EFCore/CarRentalDbContext.cs
@@ -33,7 +33,13 @@ namespace CarRental.DAL.EFCore
             modelBuilder.Entity<User>()
                 .HasMany<BookingReport>(r => r.Reports)
                 .WithOne(u => u.User)
-                .HasForeignKey(u => u.UserId); //onDelete
+                .HasForeignKey(u => u.UserId);
+            modelBuilder.Entity<User>()
+                .Property(u => u.FirstName)
+                .IsRequired();
+            modelBuilder.Entity<User>()
+                .Property(u => u.LastName)
+                .IsRequired();
 
             modelBuilder.Entity<Car>()
                 .HasMany<BookingReport>(r => r.Reports)
@@ -49,26 +55,45 @@ namespace CarRental.DAL.EFCore
                 .HasMany<Car>(c => c.Cars)
                 .WithOne(b => b.Brand)
                 .HasForeignKey(b => b.BrandId);
+            modelBuilder.Entity<CarBrand>()
+                .Property(cb => cb.Name)
+                .IsRequired();
 
             modelBuilder.Entity<RentalPoint>()
                 .HasMany<Car>(c => c.Cars)
                 .WithOne(p => p.RentalPoint)
                 .HasForeignKey(p => p.RentalPointId);
+            modelBuilder.Entity<RentalPoint>()
+                .Property(rp => rp.Name)
+                .IsRequired();
 
             modelBuilder.Entity<Location>()
                 .HasOne(rp => rp.RentalPoint)
                 .WithOne(l => l.Location)
                 .HasForeignKey<RentalPoint>(l => l.LocationId);
+            modelBuilder.Entity<Location>()
+                .Property(l => l.Address)
+                .IsRequired();
 
             modelBuilder.Entity<City>()
                 .HasMany(l => l.Locations)
                 .WithOne(c => c.City)
                 .HasForeignKey(c => c.CityId);
+            modelBuilder.Entity<City>()
+                .Property(c => c.Name)
+                .IsRequired();
 
             modelBuilder.Entity<Country>()
                 .HasMany(ci => ci.Cities)
                 .WithOne(co => co.Country)
                 .HasForeignKey(co => co.CountryId);
+            modelBuilder.Entity<Country>()
+                .Property(c => c.Name)
+                .IsRequired();
+
+            modelBuilder.Entity<BookingReport>()
+                .Property(br => br.Status)
+                .IsRequired();
         }
     }
 }
