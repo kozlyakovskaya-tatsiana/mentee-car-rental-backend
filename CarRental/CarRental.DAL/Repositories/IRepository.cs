38dbf9a2737eb730461287f6ee5e7c5598ddd973
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2021-12-16 11:35:53 +0300

    Implemented Repository Pattern Interface

---
+++ CarRental/CarRental.DAL/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarRental.DAL.Repositories
+{
+    public interface IRepository<TEntity> : IDisposable
+        where TEntity : class
+    {
+        IEnumerable<TEntity> GetEntityList();
+        void Create(TEntity item);
+        IQueryable<TEntity> GetAll();
+        TEntity Get(Guid id);
+        void Update(TEntity item);
+        void Remove(Guid id);
+        void Save();
+    }
+}
