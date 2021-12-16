eb7b80bba7a8b8e6199301b4c9828012dcbeb3eb
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2021-12-16 11:37:42 +0300

    Unfinished Rental Point repository

---
+++ CarRental/CarRental.DAL/Repositories/Models/RentalPointRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CarRental.DAL.Entities;
+
+namespace CarRental.DAL.Repositories.Models
+{
+    class RentalPointRepository : IRepository<RentalPoint>, IDisposable
+    {
+        private CarRentalDbContext _carRentalDbContext;
+        public void Dispose()
+        {
+            _carRentalDbContext.Dispose();
+        }
+
+        public IEnumerable<RentalPoint> GetEntityList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(RentalPoint item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<RentalPoint> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public RentalPoint Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(RentalPoint item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            _carRentalDbContext.SaveChanges();
+        }
+    }
+}
