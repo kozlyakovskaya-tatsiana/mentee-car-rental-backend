e0487511185b1c86bc19be84f7ce577cd2f16412
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2021-12-17 10:08:06 +0300

    Created Booking Report entity configuration

---
+++ CarRental/CarRental.DAL/EntityConfiguration/BookingReportEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿using CarRental.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CarRental.DAL.EntityConfiguration
+{
+    class BookingReportEntityRepository : IEntityTypeConfiguration<BookingReportEntity>
+    {
+        public void Configure(EntityTypeBuilder<BookingReportEntity> builder)
+        {
+            builder
+                .HasKey(br => br.Id);
+            builder
+                .Property(br => br.Status)
+                .IsRequired();
+        }
+    }
+}
