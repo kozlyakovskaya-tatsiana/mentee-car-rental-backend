39799e710448ed88f610d33f0bf13d5c1a528add
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2021-12-17 10:07:44 +0300

    Created Attachment entity configuration

---
+++ CarRental/CarRental.DAL/EntityConfiguration/AttachmentEntityConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using CarRental.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CarRental.DAL.EntityConfiguration
+{
+    public class AttachmentEntityConfiguration : IEntityTypeConfiguration<AttachmentEntity>
+    {
+        public void Configure(EntityTypeBuilder<AttachmentEntity> builder)
+        {
+            builder
+                .HasKey(a => a.Id);
+        }
+    }
+}
