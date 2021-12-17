ab0ede34dd70b429a8e2818a3c0fc61ced404fe5
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2021-12-17 08:10:50 +0300

    Car repository implementation

---
+++ CarRental/CarRental.DAL/Repositories/CarRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using CarRental.DAL.EFCore;
+using CarRental.DAL.Entities;
+using CarRental.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRental.DAL.Repositories
+{
+    public class CarRepository : ICarRepository
+    {
+        private CarRentalDbContext _db;
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Create(Car item)
+        {
+            _db.Cars.Add(item);
+        }
+
+        public IQueryable<Car> GetAll()
+        {
+            return _db.Cars;
+        }
+
+        public Car Get(Guid id)
+        {
+            return _db.Cars.Find(id);
+        }
+
+        public void Update(Car item)
+        {
+            _db.Entry(item).State = EntityState.Modified;
+        }
+
+        public void Remove(Guid id)
+        {
+            var car = _db.Cars.Find(id);
+            if (car != null)
+            {
+                _db.Cars.Remove(car);
+            }
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
