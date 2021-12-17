c04aef25be4a99d826b654087b91a227c652d2e5
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2021-12-17 09:54:08 +0300

    Created Car entity configuration

---
+++ CarRental/CarRental.DAL/EntityConfiguration/CarEntityConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using CarRental.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CarRental.DAL.EntityConfiguration
+{
+    public class CarEntityConfiguration : IEntityTypeConfiguration<CarEntity>
+    {
+        public void Configure(EntityTypeBuilder<CarEntity> builder)
+        {
+            builder
+                .HasKey(c => c.Id);
+            builder
+                .HasMany(r => r.Reports)
+                .WithOne(c => c.Car)
+                .HasForeignKey(c => c.CarId);
+            builder
+                .HasMany(a => a.Photos)
+                .WithOne(c => c.Car)
+                .HasForeignKey(c => c.CarId);
+        }
+    }
+}
