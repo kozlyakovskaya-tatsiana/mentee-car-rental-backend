ac388ecfc6bc63983fabab74f6cbd35ef2a6a918
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2021-12-17 10:08:39 +0300

    Created Location entity configuration

---
+++ CarRental/CarRental.DAL/EntityConfiguration/LocationEntityConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using CarRental.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CarRental.DAL.EntityConfiguration
+{
+    public class LocationEntityConfiguration : IEntityTypeConfiguration<LocationEntity>
+    {
+        public void Configure(EntityTypeBuilder<LocationEntity> builder)
+        {
+            builder
+                .HasKey(l => l.Id);
+            builder
+                .HasOne(rp => rp.RentalPoint)
+                .WithOne(l => l.Location)
+                .HasForeignKey<RentalPointEntity>(l => l.LocationId);
+            builder
+                .Property(l => l.Address)
+                .IsRequired();
+        }
+    }
+}
