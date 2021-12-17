2bf3c1d9ff0c120c8a342ff0f9d55d35b44596ec
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2021-12-17 08:17:03 +0300

    City repository implementation

---
+++ CarRental/CarRental.DAL/Repositories/CityRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using CarRental.DAL.EFCore;
+using CarRental.DAL.Entities;
+using CarRental.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRental.DAL.Repositories
+{
+    public class CityRepository : ICityRepository
+    {
+        private CarRentalDbContext _db;
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Create(City item)
+        {
+            _db.Cities.Add(item);
+        }
+
+        public IQueryable<City> GetAll()
+        {
+            return _db.Cities;
+        }
+
+        public City Get(Guid id)
+        {
+            return _db.Cities.Find(id);
+        }
+
+        public void Update(City item)
+        {
+            _db.Entry(item).State = EntityState.Modified;
+        }
+
+        public void Remove(Guid id)
+        {
+            var city = _db.Cities.Find(id);
+            if (city != null)
+            {
+                _db.Cities.Remove(city);
+            }
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
