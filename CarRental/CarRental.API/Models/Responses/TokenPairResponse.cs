411b49c4cea2751cf430dfa88d09f99acde8213b
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2021-12-24 11:01:28 +0300

    Created token pair mapable request and model

---
+++ CarRental/CarRental.API/Models/Responses/TokenPairResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using CarRental.DAL.Entities;
+
+namespace CarRental.API.Models.Responses
+{
+    public class TokenPairResponse
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
---
+++ CarRental/CarRental.Business/Models/TokenPairModel.cs
@@ -0,0 +1,11 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using CarRental.DAL.Entities;
+
+namespace CarRental.Business.Models
+{
+    public class TokenPairModel
+    {
+        public JwtSecurityToken AccessToken { get; set; }
+        public RefreshTokenEntity RefreshToken { get; set; }
+    }
+}
