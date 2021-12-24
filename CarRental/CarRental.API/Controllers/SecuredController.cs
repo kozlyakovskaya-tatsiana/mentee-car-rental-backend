f6dab89beff16872088646bb9ffd7ed317799100
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2021-12-24 11:02:12 +0300

    Added secured controller for auth tests

---
+++ CarRental/CarRental.API/Controllers/SecuredController.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace carRental.API.Controllers
+{
+    [Authorize]
+    [Route("api/secured")]
+    [ApiController]
+    public class SecuredController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetSecuredData()
+        {
+            return Ok("This Secured Data is available only for Authenticated Users.");
+        }
+    }
+}
