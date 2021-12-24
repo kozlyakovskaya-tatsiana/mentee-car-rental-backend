4f60eb725843269fb4b28e2ddfc1a35906fb05da
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2021-12-24 11:01:02 +0300

    Created login/register mapable models and requests

---
+++ CarRental/CarRental.API/Mapping/MappingProfile.cs
@@ -0,0 +1,30 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using AutoMapper;
+using CarRental.API.Models.Requests;
+using CarRental.API.Models.Responses;
+using CarRental.Business.Models;
+using CarRental.Business.Models.User;
+
+namespace CarRental.API.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<RegisterRequest, RegisterModel>()
+                .ForMember(u => u.Username,
+                    opt => opt
+                        .MapFrom(ur => ur.Email));
+            CreateMap<LoginRequest, LoginModel>();
+            CreateMap<TokenPairModel, TokenPairResponse>()
+                .ForMember(j => j.AccessToken,
+                    jr => jr
+                        .MapFrom(jrs =>
+                            new JwtSecurityTokenHandler().WriteToken(jrs.AccessToken)))
+                .ForMember(r => r.RefreshToken,
+                    sr => sr
+                        .MapFrom(srr => srr.RefreshToken.Token));
+            CreateMap<TokenPairRequest, TokenPairModel>();
+        }
+    }
+}
---
+++ CarRental/CarRental.Business/Mapping/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using CarRental.Business.Models;
+using CarRental.Business.Models.User;
+using CarRental.DAL.Entities;
+
+namespace CarRental.Business.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public override string ProfileName => "BLMappings";
+
+        public MappingProfile()
+        {
+            CreateMap<RegisterModel, UserEntity>().ForMember(u => u.UserName,
+                opt => opt.MapFrom(ur => ur.Email));
+            CreateMap<RefreshTokenEntity, TokenPairModel>()
+                .ForMember(x => x.RefreshToken,
+                    xr => xr.MapFrom(az => az.Token));
+                CreateMap<LoginModel, UserEntity>();
+        }
+    }
+}
