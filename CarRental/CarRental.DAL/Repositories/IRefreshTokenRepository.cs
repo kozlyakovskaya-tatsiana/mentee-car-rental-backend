8c5ed0ed65d4ee97f325a933d18cb09511477ea0
Alexandr <bazylchik.a@gmail.com>
2021-12-25 17:38:12 +0300

    Created get method for refresh tokens by user Id

--- CarRental/CarRental.DAL/Repositories/IRefreshTokenRepository.cs
+++ CarRental/CarRental.DAL/Repositories/IRefreshTokenRepository.cs
@@ -6,7 +6,7 @@ namespace CarRental.DAL.Repositories
 {
     public interface IRefreshTokenRepository :IBaseRepository<RefreshTokenEntity>
     {
-        Task<RefreshTokenEntity> Get(string token);
-        void Revoke(Guid id);
+        Task<RefreshTokenEntity> Get(Guid userId, string token);
+        Task Revoke(Guid id);
     }
 }
--- CarRental/CarRental.DAL/Repositories/Implementation/RefreshTokenRepository.cs
+++ CarRental/CarRental.DAL/Repositories/Implementation/RefreshTokenRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using CarRental.DAL.EFCore;
 using CarRental.DAL.Entities;
 using Microsoft.EntityFrameworkCore;
@@ -10,19 +11,26 @@ namespace CarRental.DAL.Repositories.Implementation
     public class RefreshTokenRepository : BaseRepository<RefreshTokenEntity>, IRefreshTokenRepository
     {
         private readonly DbSet<RefreshTokenEntity> _dbSet;
+        private readonly CarRentalDbContext _context;
         public RefreshTokenRepository(CarRentalDbContext context) : base(context)
         {
-            _dbSet = context.RefreshTokens;
+            _context = context;
+            _dbSet = _context.RefreshTokens;
         }
 
-        public Task<RefreshTokenEntity> Get(string token)
+        public Task<RefreshTokenEntity> Get(Guid userId, string token)
         {
-            return _dbSet.FirstOrDefaultAsync(u => u.Token == token);
+            return _dbSet.FirstOrDefaultAsync(u => u.UserId == userId && u.Token == token);
         }
 
-        public void Revoke(Guid id)
+        public async Task Revoke(Guid id)
         {
-            _dbSet.Remove(_dbSet.FindAsync(id).Result);
+            var collection = _dbSet.AsQueryable().Where(x => x.UserId == id);
+            foreach (var entity in collection)
+            {
+                _dbSet.Remove(entity);
+            }
+            await _context.SaveChangesAsync();
         }
     }
 }
