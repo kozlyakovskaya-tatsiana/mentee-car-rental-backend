859398fbd70a10dcd6109f018b80206494927695
Alexandr <bazylchik.a@gmail.com>
2021-12-25 17:37:30 +0300

    Created models for tokens on BLL

---
+++ CarRental/CarRental.Business/Models/Token/TokenPairModel.cs
@@ -0,0 +1,11 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using CarRental.DAL.Entities;
+
+namespace CarRental.Business.Models.Token
+{
+    public class TokenPairModel
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
