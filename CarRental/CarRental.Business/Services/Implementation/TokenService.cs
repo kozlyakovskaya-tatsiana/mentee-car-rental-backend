6f1a5070859b0237c3149d8f4c1735b2eb17239f
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2021-12-27 09:40:57 +0300

    Token service implementation

--- CarRental/CarRental.Business/Services/Implementation/TokenService.cs
+++ CarRental/CarRental.Business/Services/Implementation/TokenService.cs
@@ -5,17 +5,11 @@ using System.Linq;
 using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using AutoMapper;
-using CarRental.Business.Models;
 using CarRental.Business.Models.Token;
-using CarRental.Business.Models.User;
 using CarRental.Business.Options;
-using CarRental.DAL;
 using CarRental.DAL.Entities;
 using CarRental.DAL.Repositories;
-using CarRental.DAL.Repositories.Implementation;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 
@@ -24,23 +18,24 @@ namespace CarRental.Business.Services.Implementation
     public class TokenService : ITokenService
     {
         private readonly JwtOptions _jwt;
-
+        private readonly IRefreshTokenRepository _refreshTokenRepository;
 
         public TokenService(
-            IMapper mapper,
-            IOptions<JwtOptions> jwt
-        )
+            IOptions<JwtOptions> jwt, 
+            IRefreshTokenRepository refreshTokenRepository
+            )
         {
+            _refreshTokenRepository = refreshTokenRepository;
             _jwt = jwt.Value;
         }
 
-        /*
-        public TokenPairModel UpdateTokenPair(TokenPairModel pair)
+
+        public TokenPairModel UpdateAccessToken(TokenPairModel model)
         {
-            var token = new JwtSecurityTokenHandler().ReadJwtToken(pair.AccessToken);
+            var token = new JwtSecurityTokenHandler().ReadJwtToken(model.AccessToken);
             var claims = token.Claims;
             var userId = claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
-            var refresh = _refreshTokenRepository.Get(Guid.Parse(userId), pair.RefreshToken);
+            var refresh = _refreshTokenRepository.Get(Guid.Parse(userId), model.RefreshToken);
             if (refresh == null || IsRefreshExpired(refresh.Result))
             {
                 // You refresh token expired, re-login pls
@@ -48,84 +43,34 @@ namespace CarRental.Business.Services.Implementation
             }
             else
             {
-                var newJwt = GenerateJwt(claims);
-                pair.AccessToken = new JwtSecurityTokenHandler().WriteToken(newJwt);
-                return pair;
+                var newAccessToken = GenerateAccessToken(claims);
+                model.AccessToken = newAccessToken;
+                return model;
             }
         }
 
-        public IEnumerable<Claim> CreateUserClaims(UserEntity user)
-        {
-            var claims = new List<Claim>
-            {
-                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-                new Claim(JwtRegisteredClaimNames.Email, user.Email),
-                new Claim(JwtRegisteredClaimNames.Name, (user.FirstName + user.LastName)),
-            };
+        
 
-            return claims;
-        }
-
-        public TokenRevokeModel Revoke(TokenRevokeModel pair)
+        public TokenRevokeModel Revoke(TokenRevokeModel model)
         {
-            var token = new JwtSecurityTokenHandler().ReadJwtToken(pair.AccessToken);
+            var token = new JwtSecurityTokenHandler().ReadJwtToken(model.AccessToken);
             var id = token.Claims.FirstOrDefault(
                 x => x.Type == ClaimTypes.NameIdentifier)?.Value 
                      ?? throw new InvalidOperationException();
             _refreshTokenRepository.Revoke(Guid.Parse(id));
-            return pair;
+            return model;
         }
 
-        public JwtSecurityToken GenerateJwt(IEnumerable<Claim> claims)
-        {
-            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwt.Key));
-            var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
-            var jwtSecurityToken = new JwtSecurityToken(
-                issuer: _jwt.Issuer,
-                audience: _jwt.Audience,
-                claims: claims,
-                expires: DateTime.UtcNow.AddMinutes(_jwt.DurationInMinutes),
-                signingCredentials: signingCredentials
-                );
-            return jwtSecurityToken;
-        }
-
-
+        
         public bool IsRefreshExpired(RefreshTokenEntity refresh)
         {
             return refresh.Expired < DateTime.UtcNow;
         }
 
-        public async Task<TokenPairModel> GenerateTokenPair(LoginModel model)
-        {
-            var user = _userRepository.Get(model.Email);
-            var claims = CreateUserClaims(user);
-            var jwt = GenerateJwt(claims);
-            var refresh = GenerateRefresh(user);
-            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
-            await _refreshTokenRepository.Add(refresh);
-            var result = new TokenPairModel {AccessToken = encodedJwt, RefreshToken = refresh.Token};
-            return result;
-        }
-
-        public RefreshTokenEntity GenerateRefresh(UserEntity user)
-        {
-            var randomNumber = new byte[32];
-            using var generator = new RNGCryptoServiceProvider();
-            generator.GetBytes(randomNumber);
-            var refreshSecurityToken = new RefreshTokenEntity
-            {
-                Token = Convert.ToBase64String(randomNumber),
-                Expired = DateTime.UtcNow.AddMinutes(_jwt.RefreshTokenDurationInMinutes),
-                User = user,
-                UserId = user.Id
-            };
-            return refreshSecurityToken;
-        }
-        */
+        
         public string GenerateAccessToken(IEnumerable<Claim> claims)
         {
-            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"));
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwt.Key));
             var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
             var tokeOptions = new JwtSecurityToken(
                 issuer: _jwt.Issuer,
