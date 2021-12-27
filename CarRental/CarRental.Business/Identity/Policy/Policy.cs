0b25604f0d8561ae1eb6fd1bf97efa61b0d0019f
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2021-12-27 15:42:39 +0300

    Added policies

---
+++ CarRental/CarRental.Business/Identity/Policy/Policy.cs
@@ -0,0 +1,9 @@
+﻿namespace CarRental.Business.Identity.Policy
+{
+    public static class Policy
+    {
+        public const string AdminPolicy = "superadmin";
+        public const string ManagerPolicy = "manager";
+        public const string UserPolicy = "user";
+    }
+}
