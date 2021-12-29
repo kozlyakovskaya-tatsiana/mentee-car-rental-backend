1c4ec6c96a42e254e99f7f5e15c44f70013010e2
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2021-12-29 15:04:12 +0300

    Cleaning WEB Layer #2

--- CarRental/CarRental.API/Controllers/UserController.cs
+++ CarRental/CarRental.API/Controllers/UserController.cs
@@ -1,9 +1,4 @@
-﻿using System.Threading.Tasks;
-using AutoMapper;
-using CarRental.API.Models.Requests;
-using CarRental.Business.Models.User;
-using CarRental.Business.Services;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace CarRental.API.Controllers
 {
@@ -11,33 +6,6 @@ namespace CarRental.API.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        private readonly IAuthService _authService;
-
-        private readonly IMapper _mapper;
-
-        public UserController(
-            IMapper mapper,
-            IAuthService authService
-        )
-        {
-            _authService = authService;
-            _mapper = mapper;
-        }
-
-        [HttpPost("register")]
-        public async Task<IActionResult> SignUp(RegisterRequest userSignUpRequest)
-        {
-            var user = _mapper.Map<RegisterRequest, RegisterModel>(userSignUpRequest);
-            await _authService.Register(user);
-            return Ok();
-        }
-
-        [HttpPost("login")]
-        public async Task<IActionResult> SignIn(LoginRequest userLoginRequest)
-        {
-            var user = _mapper.Map<LoginRequest, LoginModel>(userLoginRequest);
-            var result = await _authService.Login(user);
-            return Ok(result);
-        }
+        
     }
 }
