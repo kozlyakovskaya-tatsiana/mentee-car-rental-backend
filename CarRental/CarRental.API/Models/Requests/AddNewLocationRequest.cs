f1537c9b4176e64fcf7183c539acb7e11ffc6585
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2022-02-01 14:23:26 +0300

    Major changes

--- CarRental/CarRental.API/Models/Requests/AddNewLocationRequest.cs
+++ CarRental/CarRental.API/Models/Requests/AddNewLocationRequest.cs
@@ -4,7 +4,8 @@ namespace CarRental.API.Models.Requests
 {
     public class AddNewLocationRequest
     {
-        public Guid CityId { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
         public string Address { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
--- CarRental/CarRental.Business/Models/Location/LocationModel.cs
+++ CarRental/CarRental.Business/Models/Location/LocationModel.cs
@@ -5,6 +5,8 @@ namespace CarRental.Business.Models.Location
     public class LocationModel
     {
         public Guid Id { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
         public Guid CityId { get; set; }
         public string Address { get; set; }
         public double Latitude { get; set; }
