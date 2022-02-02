f0a0e18728451977adac5e244a23e2d2be141b82
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2022-02-02 11:12:47 +0300

    Added cascade delete for entities (rental point, location)

--- CarRental/CarRental.DAL/Migrations/20220202080202_Cascade delete location with rental point.Designer.cs
+++ CarRental/CarRental.DAL/Migrations/20220202080202_Cascade delete location with rental point.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using CarRental.DAL.EFCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace CarRental.DAL.Migrations
 {
     [DbContext(typeof(CarRentalDbContext))]
-    partial class CarRentalDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220202080202_Cascade delete location with rental point")]
+    partial class Cascadedeletelocationwithrentalpoint
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- CarRental/CarRental.DAL/Migrations/20220202080731_Cascade delete rp with location.Designer.cs
+++ CarRental/CarRental.DAL/Migrations/20220202080731_Cascade delete rp with location.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using CarRental.DAL.EFCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace CarRental.DAL.Migrations
 {
     [DbContext(typeof(CarRentalDbContext))]
-    partial class CarRentalDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220202080731_Cascade delete rp with location")]
+    partial class Cascadedeleterpwithlocation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
