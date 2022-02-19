03b63f6bdf233ca89bc09903532c11f794d51a7e
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2022-02-19 16:22:37 +0300

    Created BOOK transaction

--- CarRental/CarRental.DAL/Repositories/IBookingReportRepository.cs
+++ CarRental/CarRental.DAL/Repositories/IBookingReportRepository.cs
@@ -1,8 +1,14 @@
-﻿using CarRental.DAL.Entities;
+﻿using System.Threading.Tasks;
+using CarRental.DAL.Entities;
 
 namespace CarRental.DAL.Repositories
 {
     public interface IBookingReportRepository : IBaseRepository<BookingReportEntity>
     {
+        public Task<BookingReportEntity> BookTransaction(
+            UserEntity userEntity, 
+            CarEntity carEntity, 
+            BookingReportEntity bookingReportEntity
+            );
     }
 }
